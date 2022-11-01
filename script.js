10c50df760b7eea970668a7a81cd075fa975ebc9
unknown <tay.neise@gmail.com>
2022-11-01 18:43:19 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,49 @@
+
+var number1;
+var number2;
+var result;
+
+
+function sum(){
+
+    number1= parseFloat (document.getElementById("number1").value);
+    number2= parseFloat (document.getElementById("number2").value);
+    result= number1 + number2;
+    console.log(result);
+
+    document.getElementById("lblResult").innerHTML = 'Resultado: ' + result;
+
+}
+
+function subtrac(){
+
+    number1= parseFloat (document.getElementById("number1").value);
+    number2= parseFloat (document.getElementById("number2").value);
+    result= number1 - number2;
+    console.log(result);
+
+    document.getElementById("lblResult").innerHTML = 'Resultado: ' + result;
+
+}
+
+function division(){
+
+    number1= parseFloat (document.getElementById("number1").value);
+    number2= parseFloat (document.getElementById("number2").value);
+    result= number1 / number2;
+    console.log(result);
+
+    document.getElementById("lblResult").innerHTML = 'Resultado: ' + result;
+
+}
+
+function mutiply(){
+
+    number1= parseFloat (document.getElementById("number1").value);
+    number2= parseFloat (document.getElementById("number2").value);
+    result= number1 * number2;
+    console.log(result);
+
+    document.getElementById("lblResult").innerHTML = 'Resultado: ' + result;
+
+}
