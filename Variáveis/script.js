8f1d4a80791c7634b0745a643d4cef7fc729518c
unknown <tay.neise@gmail.com>
2022-11-06 18:41:56 -0300

    Desafio variáveis

---
+++ Variáveis/script.js
@@ -0,0 +1,23 @@
+
+// Variáveis são utilizadas para armazenar um valor durante a execução de um programa. Exemplo:
+var text = 'Hello World';
+
+/* Toda variável tem:
+Nome - também chamado de identificador (observar regras de nomeclatura)
+Tipo - text, number, boolean 
+Valor - informação guardada na variável
+*/
+
+/* DESAFIO 1
+Criar uma variável de cada tipo primitivo, e imprimir estes valores no console, utilizando o método console.log();
+string
+number
+boolean
+*/
+
+
+var name = 'Taynã Neise';
+var age = 25;
+var isCustomer = true;
+
+console.log(' My name is ' + name + ", i'am " + age + ' years old. Am i a Trusted student? ' + isCustomer);
