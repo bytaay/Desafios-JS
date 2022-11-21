3403a796688fc2e63ef27007ce74ceaa9a6cf1a3
unknown <tay.neise@gmail.com>
2022-11-21 20:26:15 -0300

    contador de pares

--- Contador de Números Pares/script.js
+++ Contador de Números Pares/script.js
@@ -1,2 +1,13 @@
 // Desafio:Contador de números pares, de 0 a 100 quantos números são? 
 // Apresente o resultado no console.
+
+var total = 0;
+
+for (let i = 0; 1 < 100; i++){
+    
+    if (i%2 == 0){
+        total = total + 1;
+}
+} 
+
+console.log('De 0 a 100 temos ' + total + ' números pares')
